060013da3d2ae072d4de633af3db5a819d36480b
Jared Krinke <jared.krinke@gmail.com>
2023-05-05 11:03:44 -0700

    Export minimal HTTP server

--- min-http.lisp
+++ min-http.lisp
@@ -3,11 +3,12 @@
 (require "hunchentoot")
 
 (defpackage :min-http
-  (:use :cl))
+  (:use :cl)
+  (:export :server))
 
 (in-package :min-http)
 
-(defclass min-http-server (hunchentoot:acceptor)
+(defclass server (hunchentoot:acceptor)
   ()
   (:default-initargs
    :document-root nil
@@ -17,12 +18,12 @@
    (:documentation "Minimal, local-only HTTP server, without any resources or error messages"))
 
 ;;; Always return "not found" with an empty body
-(defmethod hunchentoot:acceptor-dispatch-request ((srv min-http-server) (request hunchentoot:request))
+(defmethod hunchentoot:acceptor-dispatch-request ((srv server) (request hunchentoot:request))
   (setf (hunchentoot:return-code hunchentoot:*reply*) hunchentoot:+http-not-found+)
   "")
 
 ;;; Return an empty body for all errors
-(defmethod hunchentoot:acceptor-status-message ((acceptor min-http-server) http-status-code &rest args)
+(defmethod hunchentoot:acceptor-status-message ((acceptor server) http-status-code &rest args)
   (declare (ignore args))
   "")
 
