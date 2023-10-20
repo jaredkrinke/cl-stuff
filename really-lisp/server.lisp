84a279aa25716f87e78bf8b995d3ac001986b733
Jared Krinke <jared.krinke@gmail.com>
2023-10-20 15:31:48 -0700

    Content replacement proof-of-concept

--- really-lisp/server.lisp
+++ really-lisp/server.lisp
@@ -20,10 +20,24 @@
   (declare (ignore args))
   "")
 
+(defun output-string (string stream)
+  "Writes a string and waits for it to be flushed"
+  (write-string string stream)
+  (finish-output stream))
+
 (defmethod hunchentoot:acceptor-dispatch-request ((server server) request)
-  (setf (hunchentoot:content-type*) "text/plain")
-  (let ((stream (hunchentoot:send-headers)))
-    (princ "Hello there!" stream)))
+  (setf (hunchentoot:content-type*) "text/html; charset=utf-8")
+  (let* ((binary-stream (hunchentoot:send-headers))
+	 (stream (flexi-streams:make-flexi-stream binary-stream :external-format :utf-8)))
+    (output-string "<!DOCTYPE html>
+<html><head><style>
+.dynamic { display: none }
+.dynamic:last-of-type { display: block }
+</style></head><body><p class=\"dynamic\">Welcome!</p>" stream)
+    (sleep 1)
+    (output-string "<p class=\"dynamic\">... to the site!</p>" stream)
+    (sleep 1)
+    (output-string "<p class=\"dynamic\">That is all.</p></body></html>" stream)))
 
 ;;; Server management
 (defvar *server* (make-instance 'server) "Instance of the server")
