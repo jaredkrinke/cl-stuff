468133582b5fdd06e7eaa5094a1a703e79c53947
Jared Krinke <jared.krinke@gmail.com>
2023-10-28 22:21:19 -0700

    Assignment

--- symbolic/exercises.lisp
+++ symbolic/exercises.lisp
@@ -663,3 +663,146 @@
 (defun hybrid-prin1 (x)
   (cond ((atom x) (format t "~s" x))
 	(t (hybrid-prin1-list x "("))))
+
+;;; Chapter 10
+(defun make-board ()
+  (list 'board 0 0 0 0 0 0 0 0 0))
+
+(defun convert-to-letter (x)
+  (ecase x
+    (0 " ")
+    (1 "O")
+    (10 "X")))
+
+(defun print-row (x y z)
+  (format t "~&    ~a | ~a | ~a"
+	  (convert-to-letter x)
+	  (convert-to-letter y)
+	  (convert-to-letter z)))
+
+(defun print-board (board)
+  (format t "~&")
+  (print-row (nth 1 board) (nth 2 board) (nth 3 board))
+  (format t "~&  -------------")
+  (print-row (nth 4 board) (nth 5 board) (nth 6 board))
+  (format t "~&  -------------")
+  (print-row (nth 7 board) (nth 8 board) (nth 9 board))
+  (format t "~%~%"))
+
+(defun make-move (player position board)
+  (setf (nth position board) player)
+  board)
+
+(defvar *board* (make-board))
+(defparameter *computer* 10)
+(defparameter *opponent* 1)
+
+(defparameter *lines*
+  '((1 2 3)
+    (4 5 6)
+    (7 8 9)
+    (1 4 7)
+    (2 5 8)
+    (3 6 9)
+    (1 5 9)
+    (3 5 7)))
+
+(defun sum-line (board triplet)
+  (+ (nth (first triplet) board)
+     (nth (second triplet) board)
+     (nth (third triplet) board)))
+
+(defun compute-sums (board)
+  (mapcar #'(lambda (triplet)
+	      (sum-line board triplet))
+	  *lines*))
+
+(defun winner-p (board)
+  (let ((sums (compute-sums board)))
+    (or (member (* 3 *computer*) sums)
+	(member (* 3 *opponent*) sums))))
+
+(defun read-a-legal-move (board)
+  (format t "~&Your move: ")
+  (let ((pos (read)))
+    (cond ((not (and (integerp pos)
+		     (<= 1 pos 9)))
+	   (format t "~&Invalid input")
+	   (read-a-legal-move board))
+	  ((not (zerop (nth pos board)))
+	   (format t "~&That space is already occupied")
+	   (read-a-legal-move board))
+	  (t pos))))
+
+(defun board-full-p (board)
+  (not (member 0 board)))
+
+(defun opponent-move (board)
+  (let* ((pos (read-a-legal-move board))
+	 (new-board (make-move *opponent* pos board)))
+    (print-board new-board)
+    (cond ((winner-p new-board) (format t "~&You win!"))
+	  ((board-full-p new-board) (format t "~&Tie game"))
+	  (t (computer-move new-board)))))
+
+(defun play-one-game ()
+  (if (y-or-n-p "Would you like to go first? ")
+      (opponent-move (make-board))
+      (computer-move (make-board))))
+
+(defun computer-move (board)
+  (let* ((best-move (choose-best-move board))
+	 (pos (first best-move))
+	 (strategy (second best-move))
+	 (new-board (make-move *computer* pos board)))
+    (format t "~&My move: ~s" pos)
+    (format t "~&My strategy: ~a~%" strategy)
+    (print-board new-board)
+    (cond ((winner-p new-board) (format t "~&I win!"))
+	  ((board-full-p new-board) new-board (format t "Tie game"))
+	  (t (opponent-move new-board)))))
+
+(defun pick-random-empty-position (board)
+  (let ((pos (+ 1 (random 9))))
+    (if (zerop (nth pos board))
+	pos
+	(pick-random-empty-position board))))
+
+(defun random-move-strategy (board)
+  (list (pick-random-empty-position board)
+	"random move"))
+
+(defun choose-best-move (board)
+  (random-move-strategy board))
+
+(defun make-three-in-a-row (board)
+  (let ((pos (win-or-block board
+			   (* 2 *computer*))))
+    (and pos (list pos "make three in a row"))))
+
+(defun block-opponent-win (board)
+  (let ((pos (win-or-block board
+			   (* 2 *computer*))))
+    (and pos (list pos "block opponent"))))
+
+(defun win-or-block (board target-sum)
+  (let ((triplet (find-if (lambda (trip) (equal (sum-line board trip)
+						target-sum))
+			  *lines*)))
+    (when triplet (find-empty-position board triplet))))
+
+(defun find-empty-position (board squares)
+  (find-if (lambda (pos) (zerop (nth pos board)))
+	   squares))
+
+(defun choose-best-move (board)
+  (or (make-three-in-a-row board)
+      (block-opponent-win board)
+      (random-move-strategy board)))
+
+(defun ugly-fixed (x y)
+  (let* ((larger (max x y))
+	 (average (/ (+ x y) 2.0))
+	 (percentage (* 100 (/ average larger))))
+    (list 'average average 'is percentage 'percent 'of 'max larger)))
+
