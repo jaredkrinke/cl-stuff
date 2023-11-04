5321c47afa7b902dee2126c30e6bd4d3e7fab981
Jared Krinke <jared.krinke@gmail.com>
2023-11-04 09:45:09 -0700

    Macros and compilation

--- symbolic/exercises.lisp
+++ symbolic/exercises.lisp
@@ -1109,3 +1109,139 @@
 	    (cell variables (rest cell)))
 	   ((null (rest cell)) (nreverse result))
 	 (push `(setq ,(first cell) ',(second cell)) result))))
+
+(defstruct (fsm-node (:print-function print-fsm-node))
+  (name nil)
+  (inputs nil)
+  (outputs nil))
+
+(defun print-fsm-node (node stream depth)
+  (format stream "#<Node ~A>"
+	  (fsm-node-name node)))
+
+(defstruct (arc (:print-function print-arc))
+  (from nil)
+  (to nil)
+  (label nil)
+  (action nil))
+
+(defun print-arc (arc stream depth)
+  (format stream "#<ARC ~A / ~A / ~A>"
+	  (fsm-node-name (arc-from arc))
+	  (arc-label arc)
+	  (fsm-node-name (arc-to arc))))
+
+(defvar *nodes*)
+(defvar *arcs*)
+(defvar *current-node*)
+
+(defun initialize-fsm ()
+  (setf *nodes* nil)
+  (setf *arcs* nil)
+  (setf *current-node* nil))
+
+(defmacro defnode (name)
+  `(add-node ',name))
+
+(defun add-node (name)
+  (let ((new-node (make-fsm-node :name name)))
+    (setf *nodes* (nconc *nodes* (list new-node)))
+    new-node))
+
+(defun find-fsm-node (name)
+  (or (find name *nodes* :key #'fsm-node-name)
+      (error "No node named ~A exists." name)))
+
+(defmacro defarc (from label to &optional action)
+  `(add-arc ',from ',label ',to ',action))
+
+(defun add-arc (from-name label to-name action)
+  (let* ((from (find-fsm-node from-name))
+	 (to (find-fsm-node to-name))
+	 (new-arc (make-arc :from from
+			    :label label
+			    :to to
+			    :action action)))
+    (setf *arcs* (nconc *arcs* (list new-arc)))
+    (setf (fsm-node-outputs from)
+	  (nconc (fsm-node-outputs from)
+		 (list new-arc)))
+    (setf (fsm-node-inputs to)
+	  (nconc (fsm-node-inputs to)
+		 (list new-arc)))
+    new-arc))
+
+(defun fsm (&optional (starting-point 'start))
+  (setf *current-node* (find-fsm-node starting-point))
+  (do ()
+      ((null (fsm-node-outputs *current-node*)))
+    (one-transition)))
+
+(defun one-transition ()
+  (format t "~&State ~A. Input: "
+	  (fsm-node-name *current-node*))
+  (let* ((ans (read))
+	 (arc (find ans
+		    (fsm-node-outputs *current-node*)
+		    :key #'arc-label)))
+    (unless arc
+      (format t "~&No arc from ~A has label ~A.~%"
+	      (fsm-node-name *current-node*) ans)
+      (return-from one-transition nil))
+    (let ((new (arc-to arc)))
+      (format t "~&~A" (arc-action arc))
+      (setf *current-node* new))))
+
+(defnode start)
+(defnode have-5)
+(defnode have-10)
+(defnode have-15)
+(defnode have-20)
+(defnode have-25)
+(defnode end)
+
+(defarc start nickel have-5 "Clunk!")
+(defarc start dime have-10 "Clink!")
+(defarc start quarter have-25 "Ker-chunk!")
+(defarc start coin-return start "Nothing to return.")
+(defarc have-5 nickel have-10 "Clunk!")
+(defarc have-5 dime have-15 "Clink!")
+(defarc have-5 quarter have-5 "Quarter returned.")
+(defarc have-5 coin-return start "Returned five cents.")
+(defarc have-10 nickel have-15 "Clunk!")
+(defarc have-10 dime have-20 "Clink!")
+(defarc have-10 quarter have-10 "Quarter returned.")
+(defarc have-10 coint-return start "Returned ten cents.")
+(defarc have-15 nickel have-20 "Clunk!")
+(defarc have-15 dime have-20 "Nickel change.")
+(defarc have-15 quarter have-15 "Quarter returned.")
+(defarc have-15 gum-button end "Deliver gum.")
+(defarc have-15 coin-return start "Returned fifteen cents.")
+(defarc have-20 nickel have-20 "Nickel returned.")
+(defarc have-20 dime have-20 "Dime returned.")
+(defarc have-20 quarter have-20 "Quarter returned.")
+(defarc have-20 gum-button end "Deliver gum, nickel change.")
+(defarc have-20 mint-button end "Deliver mints.")
+(defarc have-20 coin-return start "Returned twenty cents.")
+(defarc have-25 nickel have-25 "Nickel returned.")
+(defarc have-25 dime have-25 "Dime returned.")
+(defarc have-25 quarter have-25 "Quarter returned.")
+(defarc have-25 coin-return start "Quarter returned.")
+(defarc have-25 gum-button end "Deliver gum, dime change.")
+(defarc have-25 mint-button end "Deliver mint, nickel change.")
+(defarc have-25 chocolate-bar-button end "Deliver chocolate bar.")
+
+(defun compile-arc (arc)
+  `((equal this-input ',(arc-label arc))
+    (format t "~&~a" ,(arc-action arc))
+    (,(fsm-node-name (arc-to arc)) (rest input-syms))))
+
+(defun compile-node (node)
+  `(defun ,(fsm-node-name node) (input-syms &aux (this-input (first input-syms)))
+     (cond ((null input-syms) ',(fsm-node-name node))
+	   ,@(mapcar #'compile-arc (fsm-node-outputs node))
+	   (t (error "No arc from ~a with label ~a." ',(fsm-node-name node) this-input)))))
+
+(defmacro compile-machine ()
+  `(progn
+     ,@(mapcar #'compile-node *nodes*)))
