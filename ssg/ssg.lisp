5f5cd9ceb26d20ed5933e53a4d9dd08314972c7c
Jared Krinke <jared.krinke@gmail.com>
2023-11-05 10:12:53 -0800

    String escaping

--- ssg/ssg.lisp
+++ ssg/ssg.lisp
@@ -28,10 +28,20 @@
 ;;; TODO: Is there a Common Lisp Markdown parser that supports tables and GitHub's header-to-id logic? Ideally, one that has an intermediate (possibly list) representation I could use for handling links
 
 ;;; HTML templates
-(defun escape-string (string)
-  "Escapes STRING for use in an HTML document, using entity encoding"
-  ;;; TODO
-  string)
+(defparameter *html-escapes* '((#\& . "&amp;")
+			       (#\< . "&lt;")
+			       (#\> . "&gt;")
+			       (#\' . "&apos;")
+			       (#\" . "&quot;")))
+			       
+
+(defun write-escaped-string (string &optional (stream *standard-output*))
+  "Escapes STRING for use in an HTML document, and writes it to STREAM"
+  (loop for character across string do
+    (let ((row (assoc character *html-escapes*)))
+      (if row
+	  (write-string (cdr row) stream)
+	  (write-char character stream)))))
 
 (defun keyword->tag (keyword)
   "Converts e.g. :HTML to 'html'"
@@ -40,22 +50,27 @@
 (defun process-item (stream item)
   "Renders ITEM (a list representing HTML, or a string) to the given stream"
   ;;; TODO: DOCTYPE, void tags
+  ;;; TODO: Verbatim HTML -- needed?
   (let ((tag (keyword->tag (first item)))
 	(children (rest item)))
-    (format stream "<~a" tag)
+    (write-char #\< stream)
+    (write-string tag stream)
     (loop for child = (first children)
 	  while (keywordp child) do
-	    (format stream
-		    " ~a=\"~a\""
-		    (keyword->tag child)
-		    (escape-string (second children)))
+	    (write-char #\Space stream)
+	    (write-string (keyword->tag child))
+	    (write-string "=\"" stream)
+	    (write-escaped-string (second children) stream)
+	    (write-char #\" stream)
 	    (setf children (cddr children)))
     (write-string ">" stream)
     (loop for child in children do
-      (cond ((stringp child) (write-string (escape-string child) stream))
+      (cond ((stringp child) (write-escaped-string child stream))
 	    ((listp child) (process-item stream child))
 	    (t (error "Unexpected child: ~a (~a)" child (type-of child)))))
-    (format stream "</~a>" tag)))
+    (write-string "</" stream)
+    (write-string tag stream)
+    (write-char #\> stream)))
 
 (defun html (fragment)
   "Converts a list representing an HTML document or fragment into a string that encodes the HTML document"
