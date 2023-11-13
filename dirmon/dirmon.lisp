087d71ad4f3c437b2af8d6503f7a17c5a56808b4
Jared Krinke <jared.krinke@gmail.com>
2023-11-12 20:28:18 -0800

    In-memory directory diffing

--- dirmon/dirmon.lisp
+++ dirmon/dirmon.lisp
@@ -5,21 +5,6 @@
 
 (in-package :dirmon)
 
-;;; Utilities
-(defun diff-alists (a b &key (test 'eql))
-  "Returns: rows only in A, rows in both, and rows only in B (according to TEST on each key)"
-  ;; TODO: Consider using temporary hash tables for speed
-  ;; TODO: An actual algorithm
-  (let* ((a-firsts (mapcar #'first a))
-	 (b-firsts (mapcar #'first b))
-	 (only-a (set-difference a-firsts b-firsts :test test))
-	 (both (intersection a-firsts b-firsts :test test))
-	 (only-b (set-difference b-firsts a-firsts :test test)))
-    (break)
-    (values (mapcar (lambda (first) (assoc first a)) only-a)
-	    (mapcar (lambda (first) (cons (assoc first a) (assoc first b))) both)
-	    (mapcar (lambda (first) (assoc first b)) only-b))))
-
 ;;; File system utilities
 (defun for-each-file-in-directory (directory function &key (process-file-p (constantly t)))
   "Calls FUNCTION on each file (directly) within BASE-DIRECTORY, optionally filtering out files"
@@ -40,45 +25,55 @@
 				  process-directory-p
 				  #'process-directory)))
 
-;; (defun path-relative-to (base-directory pathname)
-;;   "Returns PATHNAME relative to BASE-DIRECTORY"
-;;   ;; TODO: Can this handle deleted files?
-;;   (enough-namestring (truename pathname) (truename base-directory)))
+;;; Snapshots (just a hash map of relative file path to modification time)
+(defparameter *empty-snapshot* (make-hash-table :test 'equal))
 
-;; Data model
-(defun for-each-change (function old new)
-  "Diffs two snapshots, calling FUNCTION for each difference"
-  (multiple-value-bind (only-a both only-b) (diff-alists old new :test 'equal)
-    (loop for row in only-a do (funcall function :delete (first row) nil))
-    (loop for (row-old . row-new) in both do
-      (when (> (rest row-new) (rest row-old))
-	(funcall function :update (first row-new) (rest row-new))))
-    (loop for row in only-b do (funcall function :add (first row) (rest row))))
-  nil)
+(defun make-snapshot (directory &key (process-directory-p (constantly t)) (process-file-p (constantly t)))
+  "Creates a new directory snapshot for DIRECTORY, with optional filtering"
+  (let ((snapshot (make-hash-table :test 'equal))
+	(directory-truename (truename directory)))
+    (flet ((relative-path (file) (enough-namestring file directory-truename)))
+      (for-each-file-under-directory directory
+				     (lambda (file) (setf (gethash (relative-path file) snapshot)
+							  (file-write-date file)))
+				     :process-directory-p process-directory-p
+				     :process-file-p process-file-p))
+    snapshot))
+
+(defun for-each-difference (old new function)
+  "Calls FUNCTION with ((OR :CREATE :UPDATE :DELETE), PATHNAME) for each difference between A and B"
+  (unless old (setf old *empty-snapshot*))
+  (loop for file-old being the hash-keys in old using (hash-value time-old)
+	for time-new = (gethash file-old new)
+	do (if time-new
+	       (when (> time-new time-old)
+		 (funcall function :update file-old))
+	       (funcall function :delete file-old)))
+  (loop for file-new being the hash-keys in new
+	do (unless (gethash file-new old)
+	     (funcall function :create file-new))))
 
 ;;; Public API
 (defun for-each-change-in-directory (function directory
 				     &key previous-snapshot
 				       (process-directory-p (constantly t))
 				       (process-file-p (constantly t)))
-  "Crawls DIRECTORY and calls FUNCTION with 3 arguments:
+  "Crawls DIRECTORY and calls FUNCTION with 2 arguments:
 
-1. :ADD, :UPDATE, or :DELETE
+1. :CREATE, :UPDATE, or :DELETE
 2. PATHNAME for the file (relative to DIRECTORY)
-3. The last modification time for the file (or NIL for :DELETEs)
 
 Returns an updated snapshot (which can be passed to future calls to detect changes)."
-  (let ((snapshot nil)
-	(directory-truename (truename directory)))
-    (flet ((relative-path (file) (enough-namestring file directory-truename)))
-      (for-each-file-under-directory directory
-				     (lambda (file) (push (cons (relative-path file)
-								(file-write-date file))
-							  snapshot))
-				     :process-directory-p process-directory-p
-				     :process-file-p process-file-p)
-      (for-each-change (lambda (change-type file time)
-			 (funcall function change-type file time))
-		       previous-snapshot
-		       snapshot)
-      snapshot)))
+  (let ((snapshot (make-snapshot directory
+				 :process-directory-p process-directory-p
+				 :process-file-p process-file-p)))
+    (for-each-difference previous-snapshot snapshot function)
+    snapshot))
+
+(defun write-snapshot (snapshot &optional (stream *standard-output*))
+  "Writes SNAPSHOT out to STREAM"
+  (error "TODO: Not implemented"))
+
+(defun read-snapshot (&optional (stream *standard-input*))
+  "Reads a snapshot (prevoiusly written using WRITE-SNAPSHOT) from STREAM"
+  (error "TODO: Not implemented"))
